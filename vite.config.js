b505203d123efa688ef54aef3e13d7cc444b5752
Vikash pandit <vikash.pandit0444@gmail.com>
2025-07-02 05:49:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'your-repo-name' with your actual repo name
 export default defineConfig({
   plugins: [react()],
+  base: '/Reusable-Card-component/',
 })
